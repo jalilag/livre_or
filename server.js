71493129e3d3c244bfb605acb12460d7e325c6eb
Jalil <agoumi.jalil@gmail.com>
2018-07-08 13:55:26 +0200

    first

---
+++ server.js
@@ -0,0 +1,35 @@
+express = require('express');
+app = express();
+bodyParser = require("body-parser");
+session = require("express-session")
+
+app.set('views','./views');
+app.set('view engine','pug');
+
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(require('./middleware/flash'))
+
+// Routes
+app.get('/' ,(request,response) => {
+	console.log(response.locals)
+	response.render('index',{title:"kaka",message:"kaka2"});
+});
+
+app.post('/',(request,response,next) =>{
+	if (request.body.login === undefined || request.body.login === '') {
+		request.flash("error","il y a une erreur");
+	}
+	response.redirect('/');
+});
+
+app.listen(8080);
